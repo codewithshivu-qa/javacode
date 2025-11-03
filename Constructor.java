eb9f7014bb65cb3b990b1e959b872f4455b3ba43
codewithshivu-qa <shivumv1016@gmail.com>
2025-11-03 20:24:41 +0530

    practise on basic java

---
+++ Constructor.java
@@ -0,0 +1,20 @@
+package basics;
+public class Constructor 
+{
+	
+	Constructor()
+	{
+		int a=20;
+		int b=30;
+		int result = a + b;
+		System.out.println("result is :"+result);
+		
+	}
+	public static void main(String[] args)
+	{
+		Constructor s1 = new Constructor();
+		
+	}
+	
+
+}
