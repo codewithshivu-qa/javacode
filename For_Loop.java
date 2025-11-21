f2d64e1e8e3cfdb21ad295af5a9f79593294b528
codewithshivu-qa <shivumv1016@gmail.com>
2025-11-21 18:41:43 +0530

    sending java code

---
+++ For_Loop.java
@@ -0,0 +1,27 @@
+//print 0 to 10 using for loop//
+package loops;
+public class For_Loop
+{
+	public static void main(String[] args)
+	{
+		/*for(int i=0;i<11;i++)                //forward direction
+		{
+			System.out.println(i);
+		}
+		for(int i=10;i>=0;i--)              //backward direction
+		{
+			System.out.println(i);
+		}
+		
+		
+		for(int i=-5;i<=5;i++)           //print -5 to + 5//
+		{
+			System.out.println(i);
+		}*/
+	    for(int i=0;i<5;i--)
+	    {
+	    	System.out.println(i);
+	}
+
+}
+}
